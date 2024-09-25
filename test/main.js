570e9e7329827ded631a5fcff52ef39f81341b89
Oleksandr Holovin <aleksandrswork61@gmail.com>
2024-09-25 17:19:41 +0300

    added test folder to create test conceps and changes on mac, make it working for latptop

---
+++ test/main.js
@@ -0,0 +1,71 @@
+const BUTTON_TO_DISABLE = [
+  "arrow-left",
+  "arrow-right",
+  "arrow-up",
+  "arrow-down",
+];
+
+function disableButtons(exceptButton) {
+  BUTTON_TO_DISABLE.forEach((className) => {
+    const button = document.querySelector(`button.${className}`);
+    if (button && button !== exceptButton) {
+      button.disabled = true;
+    }
+  });
+}
+
+function enableButtons() {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.disabled = false;
+  });
+}
+
+function buttonPress(buttonName) {
+  console.log("buttonPress");
+  fetch("/button_event", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      button_name: buttonName,
+      action: "press",
+    }),
+  }).then((response) => response.json());
+}
+
+function buttonRelease(buttonName) {
+  console.log("buttonRelease");
+  fetch("/button_event", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      button_name: buttonName,
+      action: "release",
+    }),
+  }).then((response) => response.json());
+}
+
+function handleArrowPress(buttonName, buttonElement) {
+  disableButtons(buttonElement);
+  buttonPress(buttonName);
+}
+function handleArrowRelease(buttonName) {
+  enableButtons();
+  buttonRelease(buttonName);
+}
+
+function fetchAngle() {
+  fetch("/get_angles")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("elevation").textContent =
+        Number(data.elevation) + 90;
+      document.getElementById("azimuth").textContent = data.azimuth;
+    });
+}
+
+// setInterval(fetchAngle, 100);
